4e5bd0204fbd742c65bd8f3161c29f0c1400433a
priya8940 <122329742+priya8940@users.noreply.github.com>
2024-01-02 19:04:40 +0530

    Create reviewRoute.js

---
+++ routes/reviewRoute.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const reviewCtrl = require('../controllers/reviewCtrl');
+const reviewRoute = express.Router();
+
+reviewRoute.post('/add', reviewCtrl.addReview);
+reviewRoute.get('/', reviewCtrl.allReviews);
+reviewRoute.delete('/delete/:id', reviewCtrl.deleteReview);
+//put call updates the entry in db but returns the old object
+reviewRoute.put('/update/:id', reviewCtrl.updateReview);
+reviewRoute.put('/feedback/:id', reviewCtrl.addFeedback);
+
+module.exports = reviewRoute;
